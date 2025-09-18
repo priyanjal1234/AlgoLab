3ffeb463a32933e5915dc8b4229bffc0442d0c05
Priyanjal Saxena <priyanjalsaxena1234@gmail.com>
2025-09-18 17:51:11 +0530

    Added google oauth in login page also

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -70,6 +70,18 @@ const Login = () => {
     }
   }
 
+  async function handleGoogleLogin() {
+    try {
+      await userService.signInWithGoogle();
+      dispatch(setLoggedin(true));
+      navigate("/problems")
+    } catch (error) {
+      console.log(
+        error instanceof Error ? error.message : "Error in google login"
+      );
+    }
+  }
+
   return (
     <div className="min-h-screen bg-black text-white flex items-center justify-center px-4">
       <div className="max-w-md w-full">
@@ -169,12 +181,7 @@ const Login = () => {
                 />
                 <span className="ml-2 text-sm text-gray-300">Remember me</span>
               </label>
-              <a
-                href="#"
-                className="text-sm text-green-400 hover:text-green-300 transition-colors"
-              >
-                Forgot password?
-              </a>
+              
             </div>
 
             {/* Submit Button */}
@@ -196,7 +203,7 @@ const Login = () => {
 
           {/* Social Login */}
           <div className="space-y-3">
-            <button className="w-full bg-gray-800 hover:bg-gray-700 border border-gray-700 py-3 rounded-lg transition-colors flex items-center justify-center space-x-2">
+            <button onClick={handleGoogleLogin} className="w-full bg-gray-800 hover:bg-gray-700 border border-gray-700 py-3 rounded-lg transition-colors flex items-center justify-center space-x-2">
               <svg className="w-5 h-5" viewBox="0 0 24 24">
                 <path
                   fill="currentColor"
